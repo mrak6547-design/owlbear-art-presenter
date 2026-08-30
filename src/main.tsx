8713aa81ec3f8ee4f34df8762aaa44e0e44c62d8
mrak6547-design <mrak6547@gmail.com>
2026-08-30 22:07:29 +1100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import { ActionPanel } from "./pages/ActionPanel";
+import { ViewerPage } from "./pages/ViewerPage";
+
+const params = new URLSearchParams(window.location.search);
+const mode = params.get("mode");
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {mode === "viewer" ? (
+      <ViewerPage />
+    ) : (
+      <ActionPanel embedded />
+    )}
+  </React.StrictMode>,
 );
